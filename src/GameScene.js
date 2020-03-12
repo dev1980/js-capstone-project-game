ab266e71aa925ea917a7699efee43a2840b50397
dev1980 <brham1980@gmail.com>
2020-03-12 19:20:58 +0545

    create preload method

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,8 +1,14 @@
 import { Scene } from 'phaser';
 
 class GameScene extends Scene {
+  preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('player', 'assets/player.png');
+  }
 
-
+  
 }
 
 export default GameScene;
--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 import Phaser from 'phaser';
 import GameScene from './GameScene';
+
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: GameScene,
-    physics: {
-        default: 'arcade',
-        }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: GameScene,
+  physics: {
+    default: 'arcade',
+  },
 };
 
-export { config }
+export { config };
