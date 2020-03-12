050953138167399bcf574f8dd8bf2e3af2930700
dev1980 <brham1980@gmail.com>
2020-03-12 19:36:19 +0545

    create platform for game

--- src/GameScene.js
+++ src/GameScene.js
@@ -5,10 +5,21 @@ class GameScene extends Scene {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('player', 'assets/player.png');
+    this.load.image('player', 'assets/player.png');
   }
 
-  
+  create() {
+    const sky = this.add.image(0, 0, 'sky');
+    sky.setOrigin(0, 0);
+    this.createPlatforms();
+    
+  }
+
+  createPlatforms() {
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+}
+
 }
 
 export default GameScene;
