1716d9a3b5e1a2988cec798c3f65858cf02b5588
dev1980 <brham1980@gmail.com>
2020-03-23 17:49:40 +0545

    updat the readme file

--- README.md
+++ README.md
@@ -22,16 +22,9 @@ JavaScript Game using Phaser 3
 We need [Node.js](https://nodejs.org) to install and run scripts.
 
 ## Install and run
-
-`npm install webpack webpack-cli -g`
-
-`npm update`
-
-Run next commands in your terminal:
-
 | Command | Description |
 |---------|-------------|
-| `npm install` | Install dependencies and launch browser with examples.|
+| `npm install` | Install dependencies.|
 | `npm start` | Launch browser to show the examples. <br> Press `Ctrl + c` to kill **http-server** process. |
 
 ---
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,50 @@
+
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+
   entry: './src/index.js',
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: 'game.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: 'project.bundle.js',
   },
+
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets',
+          },
+        },
+      },
       {
         test: [/\.vert$/, /\.frag$/],
         use: 'raw-loader',
       },
     ],
   },
+
   plugins: [
     new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
   ],
+
 };
