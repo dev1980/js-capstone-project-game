54a4174a5440fd20735e19d9b0b19f7e859c4315
dev1980 <brham1980@gmail.com>
2020-03-24 20:21:49 +0545

    fixed linter error

--- src/Scenes/displayScore.js
+++ src/Scenes/displayScore.js
@@ -27,6 +27,7 @@ export default class DisplayScore extends Phaser.Scene {
       const result = await response.json();
       return result;
     } catch (error) {
+        return({ msg: error })
     }
     return undefined;
   }
